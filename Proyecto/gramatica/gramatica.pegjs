e24c7c0c074e96f184521c462bb72411443f2c4e
fperuch <98467208+fperuch@users.noreply.github.com>
2024-12-06 17:02:06 -0600

    Creando sitio web

---
+++ Proyecto/gramatica/gramatica.pegjs
@@ -0,0 +1,3 @@
+inicio=_ numero _
+numero=[0-9]+
+_=[\t\n\r]*
---
+++ Proyecto/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import parseInput from "$lib/analizador";
+    import CodeMirror from "svelte-codemirror-editor"
+
+    let texto = $state("");
+    let resultado = $state("");
+    
+    function analizar() {
+        resultado = parseInput(texto);
+    }
+</script>
+
+<div class="p-5 flex flex-row h-full max-h-full bg-slate-900">
+    <div class="text-cyan-600 size-8 font-extrabold max-w-screen-sm">
+        <h1>OCL2   </h1>
+    </div>
+    <div class="h-full w-1/2 mx-2 rounded-md font-extrabold border-white bg-amber-200 border overflow-y-scroll">
+        <CodeMirror bind:value={texto} on:change={analizar}/>
+    </div>
+    <div class="rounded-md border-white bg-amber-200 border-2 h-full w-1/2 mx-2">
+        <p class="font-extrabold">
+            {resultado}
+        </p>
+    </div>
+</div>
